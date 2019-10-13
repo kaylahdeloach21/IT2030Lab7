497ffe8dc4c8e53f83ca9038e99405e8466e2db5
Kaylah DeLoach <46730990+kaylahdeloach21@users.noreply.github.com>
2019-10-13 15:45:30 -0400

    Create MVCMusicStore(Artist).cs

---
+++ MVCMusicStore(Artist).cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStoreApplication.Models
+{
+    public class Artist
+    {
+        public virtual int ArtistId { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
